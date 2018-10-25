2b05f0b3c301d38566f2ebec84bf3fdc94905d10
AG <804507289@qq.com>
2018-10-25 20:03:29 +0800

    Update Guess digit.c

---
+++ Guess digit.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+	int number;
+	int digit;
+	int result = 0;
+	
+	scanf("%d", &number);
+	while (number > 0)
+	{
+		digit = number % 10;
+		result = 10 * result + digit;
+		printf("number=%d, digit=%d, result=%d\n", number, digit, result);
+		number /= 10;
+	}
+	printf("%d\n", result);
+	
+	return 0;
+}
