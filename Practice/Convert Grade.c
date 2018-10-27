da3e584708b42380b730188b464fda898f98cbfe
AG <804507289@qq.com>
2018-10-27 23:44:14 +0800

    Update Practice/Convert Grade.c

--- Practice/Convert Grade.c
+++ Practice/Convert Grade.c
@@ -1,3 +1,11 @@
+/*
+输入成绩输出等级
+90-100	A
+80-89	B
+70-79	C
+60-69	D
+0-59	E 
+*/ 
 #include <stdio.h>
 
 int main()
@@ -6,7 +14,7 @@ int main()
 	char *level;
 	printf("请输入你的成绩:");
 	scanf("%d", &score);
-	switch (score / 10)
+	switch (score / 10)  // 发现给成绩除以10 得到的个位数就能对应不同的等级 
 	{
 		case 10:
 		case 9: 
